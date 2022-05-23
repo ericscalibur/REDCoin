575a0ea57f3e0f0331ec9a37e9129c568d952eb4
Eric Summers <ericscalibur@protonmail.com>
2022-05-22 23:15:07 -0500

    added TXID

--- REDCoin.py
+++ REDCoin.py
@@ -18,346 +18,346 @@ def cls():
 
 class REDBlock:
 
-	def __init__(self, previousNonce):
-		self.transactions = {}
-		self.previousNonce = previousNonce
-		self.nonce = ""
-		self.count = len(self.transactions)
+    def __init__(self, previousNonce):
+        self.transactions = {}
+        self.previousNonce = previousNonce
+        self.nonce = ""
+        self.count = len(self.transactions)
 
 class Transaction:
 
-	def __init__(self, sender, sender_public_key, receiver, receiver_public_key, amount):
-		self.sender = sender
-		self.sender_pk = sender_public_key
-		self.receiver = receiver
-		self.receiver_pk = receiver_public_key
-		self.amount = amount
-		self.timestamp = datetime.now()
-		self.ID = ""
+    def __init__(self, sender, sender_public_key, receiver, receiver_public_key, amount):
+        self.sender = sender
+        self.sender_pk = sender_public_key
+        self.receiver = receiver
+        self.receiver_pk = receiver_public_key
+        self.amount = amount
+        self.timestamp = datetime.now()
+        self.ID = ""
 
 class User:
 
-	def __init__(self, alias):
-		self.alias = alias
-		self.timestamp = datetime.now()
-		public_data = alias + self.timestamp.strftime("%m/%d/%Y, %H:%M:%S")
-		self.publickey = hashlib.sha256(public_data.encode()).hexdigest()
-		private_data = self.timestamp.strftime("%m/%d/%Y, %H:%M:%S") + alias + self.publickey
-		self.privatekey = hashlib.sha256(private_data.encode()).hexdigest()
-		self.REDCoin = 0.0
+    def __init__(self, alias):
+        self.alias = alias
+        self.timestamp = datetime.now()
+        public_data = alias + self.timestamp.strftime("%m/%d/%Y, %H:%M:%S")
+        self.publickey = hashlib.sha256(public_data.encode()).hexdigest()
+        private_data = self.timestamp.strftime("%m/%d/%Y, %H:%M:%S") + alias + self.publickey
+        self.privatekey = hashlib.sha256(private_data.encode()).hexdigest()
+        self.REDCoin = 0.0
 
 def CreateNewUser():
 
-	alias = input("Enter your Alias: ")
+    alias = input("Enter your Alias: ")
 
-	with open("UsersMaster.json", "r+") as f:
-		usersmaster = json.load(f)
+    with open("UsersMaster.json", "r+") as f:
+        usersmaster = json.load(f)
 
-		if alias in usersmaster.keys():
-			print("Alias already taken!")
-			alias = input("Enter your Alias: ")
-		else:
-			user = User(alias)
-			usersmaster[alias] = {'publickey':user.publickey, 'privatekey':user.privatekey, 'REDCoin':user.REDCoin }
-			f.seek(0)
-			json.dump(usersmaster, f, indent = 4)
+        if alias in usersmaster.keys():
+            print("Alias already taken!")
+            alias = input("Enter your Alias: ")
+        else:
+            user = User(alias)
+            usersmaster[alias] = {'publickey':user.publickey, 'privatekey':user.privatekey, 'REDCoin':user.REDCoin }
+            f.seek(0)
+            json.dump(usersmaster, f, indent = 4)
 
-	print("\nNew User Created!")
-	print("Alias: "+user.alias)
-	print("Public Key: "+user.publickey)
-	print("Private Key: "+user.privatekey)
-	print("REDCoin: "+str(user.REDCoin))
+            print("\nNew User Created!")
+            print("Alias: "+user.alias)
+            print("Public Key: "+user.publickey)
+            print("Private Key: "+user.privatekey)
+            print("REDCoin: "+str(user.REDCoin))
 
 def ViewBalance():
 
-	alias = input("What is your Alias: ")
+    alias = input("What is your Alias: ")
 
-	f = open("UsersMaster.json")
-	usersmaster = json.load(f)
-	f.close()
+    f = open("UsersMaster.json")
+    usersmaster = json.load(f)
+    f.close()
 
-	if alias in usersmaster.keys():
+    if alias in usersmaster.keys():
 
-		balance = usersmaster[alias]['REDCoin']
-		print("RedCoin: "+str(balance))
+        balance = usersmaster[alias]['REDCoin']
+        print("RedCoin: "+str(balance))
 
-	else:
-		print("Alias not found")
+    else:
+        print("Alias not found")
 
 
 def ViewPublicKey():
 
-	alias = input("What is your Alias: ")
+    alias = input("What is your Alias: ")
 
-	f = open("UsersMaster.json")
-	usersmaster = json.load(f)
-	f.close()
+    f = open("UsersMaster.json")
+    usersmaster = json.load(f)
+    f.close()
 
-	if alias in usersmaster.keys():
+    if alias in usersmaster.keys():
 
-		pk = usersmaster[alias]['publickey']
-		print("Public Key: "+pk)
+        pk = usersmaster[alias]['publickey']
+        print("Public Key: "+pk)
 
-	else:
-		print("Alias not found")
+    else:
+        print("Alias not found")
 
 
 def ViewPrivateKey():
 
-	alias = input("What is your Alias: ")
+    alias = input("What is your Alias: ")
 
-	f = open("UsersMaster.json")
-	usersmaster = json.load(f)
-	f.close()
+    f = open("UsersMaster.json")
+    usersmaster = json.load(f)
+    f.close()
 
-	if alias in usersmaster.keys():
+    if alias in usersmaster.keys():
 
-		vk = usersmaster[alias]['privatekey']
-		print("PrivateKey: "+vk)
+        vk = usersmaster[alias]['privatekey']
+        print("PrivateKey: "+vk)
 
-	else:
-		print("Alias not found")
+    else:
+        print("Alias not found")
 
 
 def NumberOfUsers():
 
-	f = open("UsersMaster.json")
-	data = json.load(f)
-	f.close()
+    f = open("UsersMaster.json")
+    usersmaster = json.load(f)
+    f.close()
 
-	print("Number of Users: "+str(len(data)))
+    print("Number of Users: "+str(len(usersmaster)))
 
 
 def ViewAllAccounts():
 
-	f = open("UsersMaster.json", "r")
-	usersmaster = json.load(f)
-	f.close()
+    f = open("UsersMaster.json", "r")
+    usersmaster = json.load(f)
+    f.close()
 
-	print("Alias:       Balance: ")
-	for key, value in usersmaster.items():
-		spaces = 13 - len(key)
-		space = " "
-		for s in range(spaces):
-			space = space +" "
-		print(key+space+str(value['REDCoin']))
+    print("Alias:       Balance: ")
+    for key, value in usersmaster.items():
+        spaces = 13 - len(key)
+        space = " "
+        for s in range(spaces):
+            space = space +" "
+        print(key+space+str(value['REDCoin']))
 
 
 def NewTransaction():
 
-	with open("UsersMaster.json", "r") as f:
-		usersmaster = json.load(f)
+    with open("UsersMaster.json", "r") as f:
+        usersmaster = json.load(f)
 
-	with open("REDChain.json", "r") as g:
-		redchain = json.load(g)
+    with open("REDChain.json", "r") as g:
+        redchain = json.load(g)
 
-	recentNonce = list(redchain)[-1]
-	recentBlock = redchain[recentNonce]
-	recentCount = recentBlock['count']
+    recentNonce = list(redchain)[-1]
+    recentBlock = redchain[recentNonce]
+    recentCount = recentBlock['count']
 
-	sender = input("Enter your Alias: ")
-	if sender in usersmaster.keys():
-		pk = input("Enter your Private Key: ")
-		if pk == usersmaster[sender]['privatekey']:
-			receiver = input("Enter Alias of Recipient: ")
-			if receiver in usersmaster.keys():
-				amt = input("Enter the Amount: ")
-				if float(amt) > 0 and float(amt) <= usersmaster[sender]['REDCoin']:
-					print("Processing..")
-					senderPublicKey = usersmaster[sender]['publickey']
-					receiverPublicKey = usersmaster[receiver]['publickey']
-					newTX = Transaction(sender, senderPublicKey, receiver, receiverPublicKey, amt)
-					newTX.timestamp = str(datetime.now())
-					hashable = str(newTX.timestamp) + sender+ senderPublicKey + pk + receiver + receiverPublicKey + amt
-					newTX.ID = hashlib.sha256(hashable.encode()).hexdigest()
-					time.sleep(5)
-					print("Submiting transaction to REDChain..")
-					if recentCount < 5:
+    sender = input("Enter your Alias: ")
+    if sender in usersmaster.keys():
+        pk = input("Enter your Private Key: ")
+        if pk == usersmaster[sender]['privatekey']:
+            receiver = input("Enter Alias of Recipient: ")
+            if receiver in usersmaster.keys():
+                amt = input("Enter the Amount: ")
+                if float(amt) > 0 and float(amt) <= usersmaster[sender]['REDCoin']:
+                    print("Processing..")
+                    senderPublicKey = usersmaster[sender]['publickey']
+                    receiverPublicKey = usersmaster[receiver]['publickey']
+                    newTX = Transaction(sender, senderPublicKey, receiver, receiverPublicKey, amt)
+                    newTX.timestamp = str(datetime.now())
+                    hashable = str(newTX.timestamp) + sender+ senderPublicKey + pk + receiver + receiverPublicKey + amt
+                    newTX.ID = hashlib.sha256(hashable.encode()).hexdigest()
+                    time.sleep(5)
+                    print("Submiting transaction to REDChain..")
+                    if recentCount < 5:
 
-						AddTransactionToCurrentBlock(recentNonce, newTX)
+                        AddTransactionToCurrentBlock(recentNonce, newTX)
 
-					elif recentCount == 5:
+                    elif recentCount == 5:
 
-						hashthis = ""
+                        hashthis = ""
 
-						for tx in recentBlock['transactions']:
-							hashthis += tx
+                        for tx in recentBlock['transactions']:
+                            hashthis += tx
 
-						newBlock = REDBlock(recentNonce)
-						newBlock.nonce = hashlib.sha256(hashthis.encode()).hexdigest()
-						AddBlockToChain(newBlock)
-						AddTransactionToCurrentBlock(newBlock.nonce, newTX)
+                            newBlock = REDBlock(recentNonce)
+                            newBlock.nonce = hashlib.sha256(hashthis.encode()).hexdigest()
+                            AddBlockToChain(newBlock)
+                            AddTransactionToCurrentBlock(newBlock.nonce, newTX)
 
-					time.sleep(3)
-					print("Transaction Confirmed!")
+                    time.sleep(3)
+                    print("Transaction Confirmed!")
                     print("TxID: "+newTX.ID)
 
-				else:
-					print("Insufficient REDCoin..")
-			else:
-				print("Alias not found..")
-		else:
-			print("Incorrect Private Key..")
-	else:
-		print("Alias not found..")
+                else:
+                    print("Insufficient REDCoin..")
+            else:
+                print("Alias not found..")
+        else:
+            print("Incorrect Private Key..")
+    else:
+        print("Alias not found..")
 
 
 def AddTransactionToCurrentBlock(nonce, tx):
 
-	with open("REDChain.json", "r") as f:
-		redchain = json.load(f)
+    with open("REDChain.json", "r") as f:
+        redchain = json.load(f)
 
-	if ComputeTransaction(tx):
+    if ComputeTransaction(tx):
 
-		redchain[nonce]['transactions'][tx.ID] = {"sender":tx.sender, "sender_public_key":tx.sender_pk, "receiver":tx.receiver, "receiver_public_key":tx.receiver_pk, "amount":tx.amount, "timestamp":tx.timestamp }
-		redchain[nonce]['count'] = len(redchain[nonce]['transactions'])
+        redchain[nonce]['transactions'][tx.ID] = {"sender":tx.sender, "sender_public_key":tx.sender_pk, "receiver":tx.receiver, "receiver_public_key":tx.receiver_pk, "amount":tx.amount, "timestamp":tx.timestamp }
+        redchain[nonce]['count'] = len(redchain[nonce]['transactions'])
 
-		with open("REDChain.json", "w") as h:
-			h.seek(0)
-			json.dump(redchain, h, indent = 4)
+    with open("REDChain.json", "w") as h:
+        h.seek(0)
+        json.dump(redchain, h, indent = 4)
 
 
 def AddBlockToChain(block):
 
-	with open("REDChain.json", "r") as f:
-		redchain = json.load(f)
+    with open("REDChain.json", "r") as f:
+        redchain = json.load(f)
 
-	redchain[block.nonce] = { "transactions": {}, "previousNonce": block.previousNonce, "nonce": block.nonce , "count": block.count }
+    redchain[block.nonce] = { "transactions": {}, "previousNonce": block.previousNonce, "nonce": block.nonce , "count": block.count }
 
-	with open("REDChain.json", "w") as g:
-		json.dump(redchain, g, indent = 4)
+    with open("REDChain.json", "w") as g:
+        json.dump(redchain, g, indent = 4)
 
 def ComputeTransaction(tx):
 
-	with open("UsersMaster.json", "r") as f2:
-		usersmaster = json.load(f2)
+    with open("UsersMaster.json", "r") as f2:
+        usersmaster = json.load(f2)
 
-	senderBalance = usersmaster[tx.sender]['REDCoin']
-	receiverBalance = usersmaster[tx.receiver]['REDCoin']
+    senderBalance = usersmaster[tx.sender]['REDCoin']
+    receiverBalance = usersmaster[tx.receiver]['REDCoin']
 
-	if senderBalance >= float(tx.amount):
+    if senderBalance >= float(tx.amount):
 
-		usersmaster[tx.receiver]['REDCoin'] = receiverBalance + float(tx.amount)
-		usersmaster[tx.sender]['REDCoin'] = senderBalance - float(tx.amount)
+        usersmaster[tx.receiver]['REDCoin'] = receiverBalance + float(tx.amount)
+        usersmaster[tx.sender]['REDCoin'] = senderBalance - float(tx.amount)
 
-		with open("UsersMaster.json", "w") as f3:
-			json.dump(usersmaster, f3, indent=4)
+        with open("UsersMaster.json", "w") as f3:
+            json.dump(usersmaster, f3, indent=4)
 
-		return True
+        return True
 
-	else:
-		return False
+    else:
+        return False
 
 
 
 def ReviewTransaction():
 
-	with open("REDChain.json", "r") as f:
-		redchain = json.load(f)
+    with open("REDChain.json", "r") as f:
+        redchain = json.load(f)
 
-	txID = input("What is the Transaction ID? ")
+        txID = input("What is the Transaction ID? ")
 
-	for nonce in redchain.keys():
+        for nonce in redchain.keys():
 
-		if txID in redchain[nonce]['transactions'].keys():
+            if txID in redchain[nonce]['transactions'].keys():
 
-			sender = redchain[nonce]['transactions'][txID]['sender']
-			receiver = redchain[nonce]['transactions'][txID]['receiver']
-			amt = redchain[nonce]['transactions'][txID]['amount']
-			time = redchain[nonce]['transactions'][txID]['timestamp']
+                sender = redchain[nonce]['transactions'][txID]['sender']
+                receiver = redchain[nonce]['transactions'][txID]['receiver']
+                amt = redchain[nonce]['transactions'][txID]['amount']
+                time = redchain[nonce]['transactions'][txID]['timestamp']
 
-			print('Sender: '+sender+"\t\tReciever: "+receiver)
-			print('REDCoin: '+amt+"\t\tTime: "+time)
-			print("Block Nonce: "+nonce)
-			print("Transaction ID: "+txID)
+                print('Sender: '+sender+"\t\tReciever: "+receiver)
+                print('REDCoin: '+amt+"\t\tTime: "+time)
+                print("Block Nonce: "+nonce)
+                print("Transaction ID: "+txID)
 
 def ReviewUserTransactionHistory():
 
-	with open("REDChain.json", "r") as f:
-		redchain = json.load(f)
+    with open("REDChain.json", "r") as f:
+        redchain = json.load(f)
 
-	alias = input("What is your Alias? ")
+        alias = input("What is your Alias? ")
 
-	resultCount = 0
+        resultCount = 0
 
-	for nonce in redchain.keys():
+        for nonce in redchain.keys():
 
-		for txID in redchain[nonce]['transactions']:
+            for txID in redchain[nonce]['transactions']:
 
-			if alias in redchain[nonce]['transactions'][txID].values():
+                if alias in redchain[nonce]['transactions'][txID].values():
 
-				s = redchain[nonce]['transactions'][txID]['sender']
-				r = redchain[nonce]['transactions'][txID]['receiver']
-				a = redchain[nonce]['transactions'][txID]['amount']
-				t = redchain[nonce]['transactions'][txID]['timestamp']
+                    s = redchain[nonce]['transactions'][txID]['sender']
+                    r = redchain[nonce]['transactions'][txID]['receiver']
+                    a = redchain[nonce]['transactions'][txID]['amount']
+                    t = redchain[nonce]['transactions'][txID]['timestamp']
 
-				print("Time: "+t+", Sender: "+s+", Receiver: "+r+", REDCoin: "+str(a)+", \nNonce: "+nonce+", txID: "+txID)
-				print(" ")
-				resultCount += 1
+                    print("Time: "+t+", Sender: "+s+", Receiver: "+r+", REDCoin: "+str(a)+", \nNonce: "+nonce+", txID: "+txID)
+                    print(" ")
+                    resultCount += 1
 
-	if resultCount == 0:
+        if resultCount == 0:
 
-		print("Alias not found")
+            print("Alias not found")
 
 # now, to clear the screen
 cls()
 
 print("Welcome to REDCoin! Choose from the following")
 while(play):
-	print("\nAvailable Commands:")
+    print("\nAvailable Commands:")
 
-	for index, cmd in enumerate(commandsList, start=1):
-		print("{}: {}".format(index, cmd))
+    for index, cmd in enumerate(commandsList, start=1):
+        print("{}: {}".format(index, cmd))
 
-	selection = input()
+    selection = input()
 
-	if selection == 'CreateNewUser' or selection == '1':
+    if selection == 'CreateNewUser' or selection == '1':
 
-		cls()
-		CreateNewUser()
+        cls()
+        CreateNewUser()
 
-	elif selection == 'ViewBalance' or selection == '2':
+    elif selection == 'ViewBalance' or selection == '2':
 
-		cls()
-		ViewBalance()
+        cls()
+        ViewBalance()
 
-	elif selection == 'ViewPublicKey' or selection == '3':
+    elif selection == 'ViewPublicKey' or selection == '3':
 
-		cls()
-		ViewPublicKey()
+        cls()
+        ViewPublicKey()
 
-	elif selection =='ViewPrivateKey' or selection == '4':
+    elif selection =='ViewPrivateKey' or selection == '4':
 
-		cls()
-		ViewPrivateKey()
+        cls()
+        ViewPrivateKey()
 
-	elif selection == "NumberOfUsers" or selection == '5':
+    elif selection == "NumberOfUsers" or selection == '5':
 
-		cls()
-		NumberOfUsers()
+        cls()
+        NumberOfUsers()
 
-	elif selection == "ViewAllAccounts" or selection == '6':
+    elif selection == "ViewAllAccounts" or selection == '6':
 
-		cls()
-		ViewAllAccounts()
+        cls()
+        ViewAllAccounts()
 
-	elif selection == "NewTransaction" or selection == '7':
+    elif selection == "NewTransaction" or selection == '7':
 
-		cls()
-		NewTransaction()
+        cls()
+        NewTransaction()
 
-	elif selection == "ReviewTransaction" or selection == '8':
+    elif selection == "ReviewTransaction" or selection == '8':
 
-		cls()
-		ReviewTransaction()
+        cls()
+        ReviewTransaction()
 
-	elif selection == 'ReviewUserTransactionHistory' or selection == '9':
+    elif selection == 'ReviewUserTransactionHistory' or selection == '9':
 
-		cls()
-		ReviewUserTransactionHistory()
+        cls()
+        ReviewUserTransactionHistory()
 
-	elif selection ==  "Quit" or selection == '10':
+    elif selection ==  "Quit" or selection == '10':
 
-		play = False
-		cls()
-		print("Goodbye..")
+        play = False
+        cls()
+        print("Goodbye..")
